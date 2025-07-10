0180854353fbbfcfbdc887b6d30a5aaf8e9f1dfe
Sailo001 <ssilastuwei@gmail.com>
2025-07-10 16:53:41 +0300

    Update walletManager.js

--- walletManager.js
+++ walletManager.js
@@ -1,15 +1,16 @@
 const { ethers } = require("ethers");
+require('dotenv').config();
 
 class WalletManager {
   constructor() {
     this.provider = new ethers.JsonRpcProvider(process.env.INFURA_URL);
-    
+
     this.wallets = {
       main: new ethers.Wallet(process.env.WALLET_MAIN_PK, this.provider),
       wallet1: new ethers.Wallet(process.env.WALLET_1_PK, this.provider),
       wallet2: new ethers.Wallet(process.env.WALLET_2_PK, this.provider)
     };
-    
+
     this.rotationIndex = 0;
     this.rotationOrder = ['main', 'wallet1', 'wallet2'];
   }
@@ -19,18 +20,6 @@ class WalletManager {
     this.rotationIndex = (this.rotationIndex + 1) % this.rotationOrder.length;
     return this.wallets[walletName];
   }
-
-  async getBalances() {
-    const balances = {};
-    for (const [name, wallet] of Object.entries(this.wallets)) {
-      const ethBalance = await this.provider.getBalance(wallet.address);
-      balances[name] = {
-        address: wallet.address,
-        eth: ethers.formatEther(ethBalance)
-      };
-    }
-    return balances;
-  }
 }
 
-module.exports = new WalletManager()
+module.exports = new WalletManager();
