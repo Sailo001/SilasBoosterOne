d487ba4ba0be7c2e5efe0881f5bac8b6feea6def
Sailo001 <ssilastuwei@gmail.com>
2025-07-10 13:32:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 require('dotenv').config();
-require('./bot');￼Enter
+const bot = require('./bot');
+const tradeEngine = require('./tradeEngine');
+
+const PORT = process.env.PORT || 3000;
+require('http').createServer(() => {}).listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+bot.init(tradeEngine);
+tradeEngine.init(bot);
